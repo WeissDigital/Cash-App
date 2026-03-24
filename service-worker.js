6872f841db5831d199a16702aedc545d625d6708
WeissDigital <digitalagencyweiss@gmail.com>
2026-03-24 19:02:46 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,13 @@
 const CACHE_NAME = 'cashapp';
 
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('activate', e => {
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+  // просто пропускаем — главное чтобы SW был
 });
